6188ba0065fef95eb405c4b24cdbc0c8bc5222f8
Tara Sophia Roshan <troshan@utexas.edu>
2024-02-03 15:16:55 -0600

    Add bootstrap css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import SpotightTab from './components/Spotlight/SpotlightTab.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <nav className="navigation-bar">
+      <SpotightTab/>
+    </nav>
     <App />
   </React.StrictMode>,
 )
