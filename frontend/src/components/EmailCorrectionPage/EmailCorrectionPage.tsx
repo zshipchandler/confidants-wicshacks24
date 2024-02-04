0416fabf7d566748de8a299026e7070dee7f61a9
zshipchandler <z.shipchandler@gmail.com>
2024-02-04 00:25:06 -0600

    nav bar and buttons

--- frontend/src/components/EmailCorrectionPage/EmailCorrectionPage.tsx
+++ frontend/src/components/EmailCorrectionPage/EmailCorrectionPage.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import EmailCorrection from '../EmailCorrection/EmailInput'
 import './EmailCorrectionPage.css'
 import NavLink from '../NavLink/NavLink';
+import CircleButton from '../CircleButton/CircleButton';
+import { BsPersonCircle } from 'react-icons/bs';
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const EmailCorrectionPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className = "full-body-container-email">
       <div className = "links">
         <NavLink href = "/emailCorrection" title = "Email"/>
         <NavLink href = "/forum" title = "Forum"/>
+        <CircleButton onClick= {() => navigate('/user')} buttonType={BsPersonCircle}/>
       </div>
-      
       <div className = "email-correct">
       <EmailCorrection />
+      </div>
+      <div className = "buttonPairs">
+       <Button className = "button">Spotlight</Button>
+       <Button className = "button">Contact</Button>
       </div>
     </div>
   );
--- frontend/src/components/User/UserPage.tsx
+++ frontend/src/components/User/UserPage.tsx
@@ -1,10 +1,24 @@
 import React, { useState } from 'react';
 import "./UserPage.css"
+import NavLink from '../NavLink/NavLink';
+import CircleButton from '../CircleButton/CircleButton';
+import { BsPersonCircle } from 'react-icons/bs';
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const UserPage = () => {
+  const navigate = useNavigate();
   return (
-    <div className="full-body-container-landing">
-       
+    <div className="full-body-container-user">
+       <div className = "links">
+        <NavLink href = "/emailCorrection" title = "Email"/>
+        <NavLink href = "/forum" title = "Forum"/>
+        <CircleButton onClick= {() => navigate('/user')} buttonType={BsPersonCircle}/>
+      </div>
+      <div className = "buttonPairs-user">
+       <Button className = "button">Spotlight</Button>
+       <Button className = "button">Contact</Button>
+      </div>
     </div>
   );
 };
